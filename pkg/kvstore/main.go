5d82ff78eef4564196279dd466d649e6b2c78a3c
Wojciech Walczak <wojciech.walczak@pm.me>
2022-02-12 17:39:11 +0100

    Introduce kvstore/service and kvstore/store

--- pkg/kvstore/main.go
+++ pkg/kvstore/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"kvstore/service"
+	"kvstore/store"
 )
 
-func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello!"))
-}
-
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", helloMuxHandler)
-
-	log.Fatal(http.ListenAndServe(":8080", r))
+	store := store.StringStore{}
+	service.RunKeyValueStoreService(store)
 }
